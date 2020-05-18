d806fc52aa068ae92555bb46524c492292ad1021
Dan habib <57772926+Snowy-27@users.noreply.github.com>
2020-05-18 14:47:48 +0200

    Add files via upload

---
+++ Discord/Main.py
@@ -0,0 +1,11 @@
+import os
+from deux import *
+
+
+color = instruction(["1 : crack wifi ", "2 : Vous avez choisi ", "3 : creer payload", "4 : lancer un listener", "5 :sqlmap"])
+
+color.fonction_principal()
+color.condition()
+print("le nombre entre est " + color.get_dan())
+
+
